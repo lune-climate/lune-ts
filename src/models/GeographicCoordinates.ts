8d5c38bd63e3bd0ce37c00b6013e56caf834d02c
rbruggem <rbruggem@users.noreply.github.com>
2022-04-07 13:55:53 +0000

    Client auto update

---
+++ src/models/GeographicCoordinates.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type GeographicCoordinates = {
+    /**
+     * Latitude
+     */
+    lat: number
+    /**
+     * Longitude
+     */
+    lon: number
+}
