ac2f29da2c6cf42bf61101d76a73cf8e198f0728
Rúben Aguiar <r.aguiar9080@gmail.com>
2022-04-07 16:21:02 +0100

    Drop unused schema information. (#20)
    * Drop unused schema information.
    
    They currently provide no value and simply polute the library with
    unnecesary data. If we want to add this information back to the
    client (to perform validations for example), these changes should
    be made on the base generator repo, and the information should be
    in the models themselves.
    
    Signed-off-by: Ruben Aguiar <r.aguiar9080@gmail.com>

--- src/schemas/$BundleSelection.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export const $BundleSelection = {
-    type: 'array',
-    contains: {
-        type: 'BundlePercentage',
-    },
-} as const
