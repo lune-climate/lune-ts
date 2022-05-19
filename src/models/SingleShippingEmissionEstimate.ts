d0f0f56c917b58ae97411c822001d19e89c0c033
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2022-05-19 19:38:29 +0100

    Client auto update (#41)
    Co-authored-by: rbruggem <rbruggem@users.noreply.github.com>

--- src/models/SingleShippingEmissionEstimate.ts
+++ src/models/SingleShippingEmissionEstimate.ts
@@ -7,4 +7,11 @@ import type { EmissionEstimate } from './EmissionEstimate'
 
 export type SingleShippingEmissionEstimate = EmissionEstimate & {
     distance: Distance
+    /**
+     * Summary of the methodology used to calculate emissions or any value which is a prerequisite.
+     *
+     * `imo_unavailable_container_ship_fallback`: the vessel IMO was not found, therefore a generic container ship emission factor has been used.
+     *
+     */
+    methodology?: Array<'imo_unavailable_container_ship_fallback'>
 }
