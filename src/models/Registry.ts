68d2dd4ad45a931a45ce2ea8b7e3f6c61969202d
rbruggem <rbruggem@users.noreply.github.com>
2022-07-29 10:23:53 +0000

    Client auto update

---
+++ src/models/Registry.ts
@@ -0,0 +1,16 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * The project's Verification Standard Entity name or equivalent organization.
+ */
+export enum Registry {
+    AMERICAN_CARBON_REGISTRY = 'American Carbon Registry',
+    VERRA = 'Verra',
+    PURO_EARTH = 'Puro.Earth',
+    GOLD_STANDARD = 'Gold Standard',
+    CLIMATE_ACTION_RESERVE = 'Climate Action Reserve',
+    WOODLAND_CARBON_CODE = 'Woodland Carbon Code',
+    __EMPTY__ = '',
+}
