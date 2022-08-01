f3d9d8dc532c934056e60a28be41951dd54c43ff
rbruggem <rbruggem@users.noreply.github.com>
2022-08-01 15:21:03 +0000

    Client auto update

--- src/models/AnyAccount.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-import type { Account } from './Account'
-import type { ClientAccount } from './ClientAccount'
-
-export type AnyAccount = Account | ClientAccount
