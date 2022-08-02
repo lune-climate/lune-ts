e48a80a411eb78916ef9a3cce911acead1e6810d
Jakub Stasiak <jakub@stasiak.at>
2022-08-02 11:21:10 +0200

    Fix mass unit usage in the examples
    Previously it would fail with
    
        test.ts:6:46 - error TS2339: Property 'Mass' does not exist on type 'typeof import("/lune-ts/example-usage/ts-usage/node_modules/@lune-climate/lune/esm/client")'.
    
        6         mass: { amount: '123.21', unit: lune.Mass.unit.T },
                                                       ~~~~
    
    Mass is a type and it only exists at compile time.

--- example-usage/ts-usage/test.ts
+++ example-usage/ts-usage/test.ts
@@ -3,7 +3,7 @@ import * as lune from '@lune-climate/lune'
 const luneClient = new lune.LuneClient('your_api_key_would_go_here')
 ;(async () => {
     const resp = await luneClient.createOrderByMass({
-        mass: { amount: '123.21', unit: lune.Mass.unit.T },
+        mass: { amount: '123.21', unit: lune.MassUnit.T },
     })
     // Handle error and success response separately
     if (resp.err) {
