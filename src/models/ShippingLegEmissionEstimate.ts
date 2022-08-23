64f23dcc3d59dac8689f14acf15a13e6ac432081
rbruggem <rbruggem@users.noreply.github.com>
2022-08-23 10:22:02 +0000

    Client auto update

--- src/models/ShippingLegEmissionEstimate.ts
+++ src/models/ShippingLegEmissionEstimate.ts
@@ -13,5 +13,5 @@ export type ShippingLegEmissionEstimate = EmissionEstimate & {
      * `imo_unavailable_container_ship_fallback`: the vessel IMO was not found, therefore a generic container ship emission factor has been used.
      *
      */
-    methodology?: Array<'imo_unavailable_container_ship_fallback'>
+    methodology: Array<'imo_unavailable_container_ship_fallback'>
 }
