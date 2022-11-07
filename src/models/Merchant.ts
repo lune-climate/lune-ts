c8bfa1f4a800359f5490c99da80e097881bb46ba
rbruggem <rbruggem@users.noreply.github.com>
2022-11-07 12:16:52 +0000

    Client auto update

--- src/models/Merchant.ts
+++ src/models/Merchant.ts
@@ -4,14 +4,6 @@
 
 export type Merchant = {
     /**
-     * An ISO 18245 Merchant Category Code (leading zeros need to be preserved) corresponding
-     * to the transaction.
-     *
-     * See https://github.com/greggles/mcc-codes for available codes.
-     *
-     */
-    categoryCode: string
-    /**
      * The name of the merchant.
      */
     name?: string
@@ -19,4 +11,29 @@ export type Merchant = {
      * The three-letter code of the merchant's country.
      */
     countryCode: string
-}
+} & (
+    | {
+          /**
+           * An ISO 18245 Merchant Category Code (leading zeros need to be preserved) corresponding
+           * to the transaction.
+           *
+           * See https://github.com/greggles/mcc-codes for available codes.
+           *
+           */
+          categoryCode: string
+      }
+    | {
+          /**
+           * A Classification of Individual Consumption According to Purpose
+           * (https://en.wikipedia.org/wiki/Classification_of_Individual_Consumption_According_to_Purpose)
+           * code indicating the type of goods or services purchased.
+           *
+           * It is the responsibility of the API user to provide a valid code here. We validate only
+           * the top level identifier (the number before the first dot). If an invalid code is provided
+           * we fall back to the division-level emission defaults (so for example 14.999999 will fall
+           * back to 14).
+           *
+           */
+          coicopCode: string
+      }
+)
