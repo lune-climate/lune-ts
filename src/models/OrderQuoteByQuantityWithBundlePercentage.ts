1d5e2347f308c1fbe4905eb3676a3bf1ecad935c
Ruben Aguiar <r.aguiar9080@gmail.com>
2022-12-22 10:52:11 +0000

    Add missing files
    Signed-off-by: Ruben Aguiar <r.aguiar9080@gmail.com>

---
+++ src/models/OrderQuoteByQuantityWithBundlePercentage.ts
@@ -0,0 +1,22 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { BundleSelectionRequest } from './BundleSelectionRequest'
+import type { Mass } from './Mass'
+import type { MassUnit } from './MassUnit'
+
+/**
+ * Order by Quantity details
+ */
+export type OrderQuoteByQuantityWithBundlePercentage = {
+    /**
+     * Mass of CO2 offsets to purchase
+     */
+    mass: Mass
+    bundleSelection?: BundleSelectionRequest
+    /**
+     * Selects to which precision to truncate the quantities assigned to each bundle.
+     */
+    quantityTrunc?: MassUnit
+}
