6b136a35ed2e1a5ef8d7490e6b6c49b42eef9958
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2023-04-30 21:45:04 +0100

    Client auto update (#218)
    Co-authored-by: rbruggem <rbruggem@users.noreply.github.com>

---
+++ src/models/Locode.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type Locode = {
+    /**
+     * UN LOCODE. For a full list of options (https://unece.org/trade/cefact/unlocode-code-list-country-and-territory)
+     */
+    locode: string
+}
