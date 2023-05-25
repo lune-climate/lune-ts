57c541c92b3b683b4d6065e430482b1576d82463
rbruggem <rbruggem@users.noreply.github.com>
2023-05-25 11:09:13 +0000

    Client auto update

--- src/models/Address.ts
+++ src/models/Address.ts
@@ -2,11 +2,20 @@
 /* tslint:disable */
 /* eslint-disable */
 
+/**
+ * Detailed information about an address.
+ *
+ * Even though though `street_line1`, `postcode`, `city` are optional properties, the more details
+ * you provide, the more likely we are able to resolve the address to its intended location.
+ *
+ * Note: only `postcode` is guaranteed to be unique for a country.
+ *
+ */
 export type Address = {
     /**
      * A street and house number (or equivalent).
      */
-    streetLine1: string
+    streetLine1?: string
     /**
      * An address component more precise than a street and house number.
      */
@@ -14,8 +23,8 @@ export type Address = {
     /**
      * The postal code in the format specific to the country it's in
      */
-    postcode: string
-    city: string
+    postcode?: string
+    city?: string
     /**
      * A three-letter country code.
      */
