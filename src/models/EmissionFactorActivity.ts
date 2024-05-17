d6f140045425a444e792098bae3f3d4413ac5a0a
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2024-05-17 16:39:43 +0100

    Client auto update (#562)
    Co-authored-by: rbruggem <rbruggem@users.noreply.github.com>

--- src/models/EmissionFactorActivity.ts
+++ src/models/EmissionFactorActivity.ts
@@ -14,11 +14,22 @@ import type { EmissionFactorDenominatorUnit } from './EmissionFactorDenominatorU
 
 export type EmissionFactorActivity = {
     /**
-     * The quantity of the activity being measured
+     * The quantity of the activity being measured.
+     *
+     * For example, if you using the `Basic iron and steel and of ferro-alloys and first products thereof` emission factor to estimate
+     * the emission of $1000 of steel pipe. Pass `"1000"` as value.
+     *
      */
     value: string
     /**
-     * The units of the activity being measured
+     * The units of the activity being measured.
+     *
+     * This property must be equal or compatible with the emission factor's denominator.
+     *
+     * For example, if you using the `Basic iron and steel and of ferro-alloys and first products thereof` emission factor, which has an intensity of `0.738411878489 kgCO2e/EUR`
+     * to estimate the emission of $1000 of steel pipe. Pass `"USD"` as unit.
+     * Where possible, we apply a conversion factor to the unit.
+     *
      */
     unit: EmissionFactorDenominatorUnit
 }
