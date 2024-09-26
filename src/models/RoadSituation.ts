b0eacface5472fd7f73f57ec744c5b0b966d751b
rbruggem <rbruggem@users.noreply.github.com>
2024-09-26 10:59:37 +0000

    Client auto update

--- src/models/RoadSituation.ts
+++ src/models/RoadSituation.ts
@@ -17,4 +17,13 @@ export enum RoadSituation {
     CITY_URBAN_FREEFLOW = 'city_urban_freeflow',
     MOTORWAY_URBAN_FREEFLOW = 'motorway_urban_freeflow',
     MOTORWAY_RURAL_FREEFLOW = 'motorway_rural_freeflow',
+    CITY_URBAN_HEAVY_TRAFFIC = 'city_urban_heavy_traffic',
+    CITY_URBAN_STOP_AND_GO = 'city_urban_stop_and_go',
+    MOTORWAY_URBAN_HEAVY_TRAFFIC = 'motorway_urban_heavy_traffic',
+    MOTORWAY_URBAN_STOP_AND_GO = 'motorway_urban_stop_and_go',
+    MOTORWAY_RURAL_HEAVY_TRAFFIC = 'motorway_rural_heavy_traffic',
+    MOTORWAY_RURAL_STOP_AND_GO = 'motorway_rural_stop_and_go',
+    ARTERIAL_ROAD_RURAL_FREEFLOW = 'arterial_road_rural_freeflow',
+    ARTERIAL_ROAD_RURAL_HEAVY_TRAFFIC = 'arterial_road_rural_heavy_traffic',
+    ARTERIAL_ROAD_RURAL_STOP_AND_GO = 'arterial_road_rural_stop_and_go',
 }
