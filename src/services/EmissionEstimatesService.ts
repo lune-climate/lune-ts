67186551b41e7dc1371513ae4872b43d7a04b66f
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2024-12-06 09:39:04 +0000

    Client auto update (#757)
    Co-authored-by: rbruggem <rbruggem@users.noreply.github.com>

--- src/services/EmissionEstimatesService.ts
+++ src/services/EmissionEstimatesService.ts
@@ -344,6 +344,37 @@ export abstract class EmissionEstimatesService {
     }
 
     /**
+     * Get a passenger transportation emission estimate
+     * Returns a passenger transportation emission estimate.
+     * @param id The estimate's unique identifier
+     * @param options Additional operation options
+     * @returns PassengerTransportationEmissionEstimate OK
+     */
+    public getPassengerTransportationEstimate(
+        id: string,
+        options?: {
+            /**
+             * Account Id to be used to perform the API call
+             */
+            accountId?: string
+        },
+    ): Promise<Result<SuccessResponse<PassengerTransportationEmissionEstimate>, ApiError>> {
+        return __request(this.client, this.config, options || {}, {
+            method: 'GET',
+            url: '/estimates/passenger-transportation/{id}',
+            path: {
+                id: id,
+            },
+            errors: {
+                401: `The API Key is missing or is invalid`,
+                404: `The specified resource was not found`,
+                429: `Too many requests have been made in a short period of time`,
+                503: `The service is temporarily unavailable. You may retry.`,
+            },
+        })
+    }
+
+    /**
      * List all shipping estimates
      * Returns single and multi-leg shipping estimates paginated in reverse order.
      *
