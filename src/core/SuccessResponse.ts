8bde8f51dc3bd87354929c3d8241e72c507cd11d
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2024-12-18 11:42:42 +0000

    Allow an unmodified SuccessResponse (#772)
    With https://github.com/lune-climate/openapi-typescript-codegen/pull/79,
    we've stopped modifying non-JSON response payloads.
    
    The change to `SuccessResponse` reflects that.

--- src/core/SuccessResponse.ts
+++ src/core/SuccessResponse.ts
@@ -16,7 +16,7 @@ type AsObject<T> = T extends object
         ? { notAnObject: T }
         : T
     : { notAnObject: T }
-export type SuccessResponse<T> = AsObject<T> & { _meta: Meta<T> }
+export type SuccessResponse<T> = (AsObject<T> & { _meta: Meta<T> }) | T
 
 function isNotPureObject(value: unknown): boolean {
     return (
