d52c520fe9ba2ceb7f5c546a3b1e5d3fbde48386
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2025-03-18 08:42:26 +0000

    Client auto update (#863)
    Co-authored-by: raguiar9080 <raguiar9080@users.noreply.github.com>

--- src/models/OptionalMerchantViaSearch.ts
+++ src/models/OptionalMerchantViaSearch.ts
@@ -10,23 +10,35 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export type OptionalMerchantViaSearch = {
-    /**
-     * The name of the merchant.
-     */
-    name?: string
-    /**
-     * The three-letter code of the merchant's country.
-     */
-    countryCode?: string
-    /**
-     * A category the transaction belongs to.
-     */
-    category?: string
-    /**
-     * The search term to query.
-     * Search is resolved using a combination of semantic and keyword searching.
-     *
-     */
-    searchTerm?: string
-}
+export type OptionalMerchantViaSearch =
+    | {
+          /**
+           * A category the transaction belongs to.
+           */
+          category: string
+      }
+    | {
+          /**
+           * A list of categories the transaction belongs to.
+           *
+           * From most specific to most general.
+           *
+           */
+          categoryHierarchy?: Array<string>
+      }
+    | {
+          /**
+           * The name of the merchant.
+           */
+          name?: string
+          /**
+           * The three-letter code of the merchant's country.
+           */
+          countryCode?: string
+          /**
+           * The search term to query.
+           * Search is resolved using a combination of semantic and keyword searching.
+           *
+           */
+          searchTerm?: string
+      }
