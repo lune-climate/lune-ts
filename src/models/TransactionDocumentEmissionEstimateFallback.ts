2b581a987955807f7db467bdd6f8175a7704d5e6
Roberto Bruggemann <1833249+rbruggem@users.noreply.github.com>
2025-03-25 10:17:05 +0000

    Client auto update (#872)
    Co-authored-by: raguiar9080 <raguiar9080@users.noreply.github.com>

--- src/models/TransactionDocumentEmissionEstimateFallback.ts
+++ src/models/TransactionDocumentEmissionEstimateFallback.ts
@@ -12,6 +12,7 @@
 
 import type { EmissionFactorWithGasEmissions } from './EmissionFactorWithGasEmissions.js'
 import type { MonetaryAmount } from './MonetaryAmount.js'
+import type { NullEnum } from './NullEnum.js'
 
 /**
  * A fallback estimate for a transaction document.
@@ -28,4 +29,29 @@ export type TransactionDocumentEmissionEstimateFallback = {
      */
     category: string
     value: MonetaryAmount
+    /**
+     * The most accurate category matching the search.
+     *
+     * The emission factor for this term is used to compute the emission estimate.
+     *
+     */
+    categoryMatch: string | null
+    /**
+     * Populated if semantic search was used to find a match to the category.
+     * Indicates confidence in the match from 0 - 2, 0 being most confident.
+     *
+     */
+    categoryMatchScore: number | null
+    /**
+     * The text embedding model used to support semantic search
+     *
+     */
+    semanticMatchingModel: string | null
+    exchangeRate: string | NullEnum
+    exchangeRateDate: string | NullEnum
+    /**
+     * The 3-character country code that has been identified.
+     */
+    countryCode: string
+    dietFactor: string | NullEnum
 }
