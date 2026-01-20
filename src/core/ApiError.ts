lune-climate/lune-ts

commit 0157ac9506d629f06f483f37416a42b1c5457944

Extract error details into ApiError (#1089)
Using the array is cumbersome and we've been returning single error in
API responses for a long while now.

This should make error handling nicer.

Why not place these things into the error property in ApiError? Because
too much unnecessary nesting.

src/core/ApiError.ts
@@ -5,9 +5,16 @@ import { ApiRequestOptions } from './ApiRequestOptions.js'
 
 export type ApiError =
     | { readonly description: string }
-    | {
+    | ({
           readonly description: string
           readonly statusCode: number
+          /**
+           * @deprecated Use `errorCode` and `message` properties, if present, to see
+           * the details of the error encountered.
+           *
+           * This array is present for backwards * compatibility and always contains
+           * a single element with data inside identical to `errorCode` and `messages`.
+           */
           readonly errors: ErrorResponse['errors'] | undefined
           /**
            * The request id as generated by the API server. The id uniquely identifies
@@ -15,7 +22,10 @@ export type ApiError =
            * to help track down errors and aid in debugging.
            */
           readonly requestId: string | undefined
-      }
+      } &
+          // Looks complicated but there's a purpose: communicate that errorCode and message
+          // are either both defined or both undefined.
+          (ErrorResponse['error'] | { errorCode: undefined; message: undefined }))
 
 export function constructApiError(error: AxiosError, options: ApiRequestOptions): ApiError {
     // The request was made and the server responded with a status code
@@ -33,10 +43,13 @@ export function constructApiError(error: AxiosError, options: ApiRequestOptions)
             ...options.errors,
         }
 
+        const errorResponse = error.response.data as ErrorResponse
         return {
             statusCode: error.response.status,
             description: errors[error.response.status],
-            errors: (error.response.data as ErrorResponse).errors,
+            errors: errorResponse.errors,
+            errorCode: errorResponse.error.errorCode,
+            message: errorResponse.error.message,
             // Implementation detail: Cloudflare sits in front of Lune API servers currently
             // and we use its cf-ray values as request ids.
             requestId: error.response.headers['cf-ray'],

commit e33eed756423fcb6399a870482152f1826184a57

Fix "can't access property "errorCode"" (#1091)
In my earlier change I forgot that the way we were asserting the type
here was a bit dangerous.

error.response.data may or may not contain data.

Accessing one property on that is fine-ish (it will just evaluate to
undefined) but accessing a nested property is not (we'll try to access
a property on undefined).

This triggered a failure[1] _in error handling_ in the dashboard, which is
ironic:

    TypeError: can't access property "errorCode", o.error is undefined
    ../../../../node_modules/@lune-climate/lune/esm/core/ApiError.js:21 - dY1

Now we the assertion reflects reality much better and we can make this
code handle missing error information gracefully. The data is expected
to not be provided with some status codes, there's no issue there.

Fixes: 0157ac9506d6 ("Extract error details into ApiError (#1089)")

[1] https://app.bugsnag.com/lune/lune-dashboard/errors/696fab684b9fce566497939b?event_id=696fab6801628f9dd3f90000&i=sk&m=nw

src/core/ApiError.ts
@@ -43,13 +43,13 @@ export function constructApiError(error: AxiosError, options: ApiRequestOptions)
             ...options.errors,
         }
 
-        const errorResponse = error.response.data as ErrorResponse
+        const errorResponse = error.response.data as ErrorResponse | undefined
         return {
             statusCode: error.response.status,
             description: errors[error.response.status],
-            errors: errorResponse.errors,
-            errorCode: errorResponse.error.errorCode,
-            message: errorResponse.error.message,
+            errors: errorResponse?.errors,
+            errorCode: errorResponse?.error.errorCode,
+            message: errorResponse?.error.message,
             // Implementation detail: Cloudflare sits in front of Lune API servers currently
             // and we use its cf-ray values as request ids.
             requestId: error.response.headers['cf-ray'],